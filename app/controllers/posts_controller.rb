be86adb8f818592f170e006f08e5c485aaa15362
EC2 Default User <ec2-user@ip-172-31-38-160.ap-southeast-1.compute.internal>
2018-10-11 14:47:07 +0000

    Done-halfwayof-tutorial-second

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,8 @@
 class PostsController < ApplicationController
   before_action :find_post, only: [:edit, :update, :show, :delete]
   
+  #This authenticates admin whenever a post is to be created, updated or destroyed.
+  before_action :authenticate_admin!, except: [:index, :show]
   
   # Index action to render all posts
   def index
